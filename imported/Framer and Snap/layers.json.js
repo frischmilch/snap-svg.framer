4ef90f0f5294338db8596645d27621c08f3f74e6
Johannes Eckert <jeckert@adobe.com>
2015-05-04 18:18:38 -0700

    committing arcs

---
+++ imported/Framer and Snap/layers.json.js
@@ -0,0 +1,175 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["Framer and Snap/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "id" : "AF474BB9-B3FF-4314-90DF-296143B02C61",
+    "visible" : true,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "413C7A97-2450-4DFD-8682-B6EE983ECA08",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-button-413C7A97-2450-4DFD-8682-B6EE983ECA08.png",
+          "frame" : {
+            "y" : 1148,
+            "x" : 212,
+            "width" : 350,
+            "height" : 81
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 1148,
+          "x" : 212,
+          "width" : 350,
+          "height" : 81
+        },
+        "name" : "button"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "BA5325CB-0E38-4D37-9B28-58B46FBF1434",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "BD5A67E0-F207-41F6-B7F0-C3AB52BD92C0",
+            "visible" : true,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "57B9F7B3-3073-479D-9EE0-B7318F749583",
+                "visible" : true,
+                "children" : [
+
+                ],
+                "image" : {
+                  "path" : "images\/Layer-Battery-57B9F7B3-3073-479D-9EE0-B7318F749583.png",
+                  "frame" : {
+                    "y" : 10,
+                    "x" : 691,
+                    "width" : 49,
+                    "height" : 20
+                  }
+                },
+                "imageType" : "png",
+                "layerFrame" : {
+                  "y" : 10,
+                  "x" : 691,
+                  "width" : 49,
+                  "height" : 20
+                },
+                "name" : "Battery"
+              }
+            ],
+            "image" : {
+              "path" : "images\/Layer-100%__Bluetooth__Battery-BD5A67E0-F207-41F6-B7F0-C3AB52BD92C0.png",
+              "frame" : {
+                "y" : 4,
+                "x" : 595,
+                "width" : 145,
+                "height" : 31
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 4,
+              "x" : 595,
+              "width" : 145,
+              "height" : 31
+            },
+            "name" : "100%__Bluetooth__Battery"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "E1617C81-5F17-4069-8001-84A9AFFD909C",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-Signal_&_Carrier-E1617C81-5F17-4069-8001-84A9AFFD909C.png",
+              "frame" : {
+                "y" : 10,
+                "x" : 12,
+                "width" : 165,
+                "height" : 19
+              }
+            },
+            "imageType" : "png",
+            "layerFrame" : {
+              "y" : 10,
+              "x" : 12,
+              "width" : 165,
+              "height" : 19
+            },
+            "name" : "Signal_&_Carrier"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-statusbar-BA5325CB-0E38-4D37-9B28-58B46FBF1434.png",
+          "frame" : {
+            "y" : 4,
+            "x" : 12,
+            "width" : 728,
+            "height" : 31
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 4,
+          "x" : 12,
+          "width" : 728,
+          "height" : 31
+        },
+        "name" : "statusbar"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "8E1D8777-F997-4413-955F-2F385A9FF0B6",
+        "visible" : true,
+        "children" : [
+
+        ],
+        "image" : {
+          "path" : "images\/Layer-steps-8E1D8777-F997-4413-955F-2F385A9FF0B6.png",
+          "frame" : {
+            "y" : 608,
+            "x" : 242,
+            "width" : 267,
+            "height" : 61
+          }
+        },
+        "imageType" : "png",
+        "layerFrame" : {
+          "y" : 608,
+          "x" : 242,
+          "width" : 267,
+          "height" : 61
+        },
+        "name" : "steps"
+      }
+    ],
+    "image" : {
+      "path" : "images\/Layer-iPhone_6-AF474BB9-B3FF-4314-90DF-296143B02C61.png",
+      "frame" : {
+        "y" : 0,
+        "x" : 0,
+        "width" : 750,
+        "height" : 1334
+      }
+    },
+    "imageType" : "png",
+    "layerFrame" : {
+      "y" : 0,
+      "x" : 0,
+      "width" : 750,
+      "height" : 1334
+    },
+    "name" : "iPhone_6"
+  }
+]
